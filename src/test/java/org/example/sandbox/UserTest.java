e8de0a910c5f332eb0a4856452a7dc123890b822
Anton Zhuk <54962677+anton-zhuk99@users.noreply.github.com>
2021-04-06 21:51:53 +0300

    Add examples.

--- src/test/java/org/example/sandbox/UserTest.java
+++ src/test/java/org/example/sandbox/UserTest.java
@@ -1,24 +1,49 @@
 package org.example.sandbox;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
 
 public class UserTest {
 
+    private static User user1;
+    private static User user2;
+    private static User user3;
+
+    @BeforeClass
+    public static void initializeUsers() {
+        user1 = new User("John", 35, Gender.MALE);
+        user2 = new User("Mary", 34, Gender.FEMALE);
+        user3 = new User("Jack", 7, Gender.MALE);
+    }
+
     @Test
-    public void helloWorldTest() {
+    public void getAllUsersTest() {
         // expected
         // actual
-        User user1 = new User("John", 35, Gender.MALE);
-        User user2 = new User("Mary", 34, Gender.FEMALE);
-        User user3 = new User("Jack", 7, Gender.MALE);
-
         List<User> expected = User.getAllUsers();
         List<User> actual = List.of(user1, user2);
 
+        Assert.assertEquals("Test if lists are equal", expected, actual);
+    }
+
+    @Test
+    public void getAllUsersByGenderTest() {
+        // QA - quality assurance
+        List<User> expected = User.getAllUsers(Gender.MALE);
+        List<User> actual = List.of(user1, user3);
+
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void failTest() {
+        boolean condition = false;
+        if (!condition) {
+            Assert.fail();
+        }
     }
 
 }
